4f3d017501dfba51d34d7f66af29717cf98fdc8e
Akshata Girkar <Akshata.Girkar@ril.com>
2022-08-09 21:01:03 +0530

    Completes HTTP call functionality

---
+++ src/app/media-item.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class MediaItemService {
+  mediaItems = [
+    {
+      id: 1,
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
+      year: 2010,
+      watchedOn: 1294166565384,
+      isFavorite: false
+    },
+    {
+      id: 2,
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: true
+    },
+    {
+      id: 3,
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: false
+    },
+    {
+      id: 4,
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
+      year: null,
+      watchedOn: null,
+      isFavorite: true
+    },
+    {
+      id: 5,
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
+      year: 2015,
+      watchedOn: 1457166565384,
+      isFavorite: false
+    }
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  get(medium) {
+    const getOptions = {
+      params: { medium: medium }
+    };
+    return this.http.get<MediaItemsResponse>("mediaitems", getOptions).pipe(
+      map((response: MediaItemsResponse) => {
+        return response.mediaItems;
+      })
+    );
+  }
+
+  add(mediaItem) {
+    return this.http.post("mediaitems", mediaItem);
+  }
+
+  delete(mediaItem) {
+    return this.http.delete(`mediaitems/${mediaItem.id}`);
+  }
+}
+interface MediaItemsResponse {
+  mediaItems: MediaItem[];
+}
+
+export interface MediaItem {
+  id: number;
+  name: string;
+  medium: string;
+  category: string;
+  year: number;
+  watchedOn: number;
+  isFavorite: boolean;
+}
