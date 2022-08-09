4ec780d1797165af1334860f0b5002fee4e21dcd
Akshata Girkar <Akshata.Girkar@ril.com>
2022-08-09 21:31:32 +0530

    Handles HTTP errors

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,59 +1,12 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { map, catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class MediaItemService {
-  mediaItems = [
-    {
-      id: 1,
-      name: "Firebug",
-      medium: "Series",
-      category: "Science Fiction",
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: "The Small Tall",
-      medium: "Movies",
-      category: "Comedy",
-      year: 2016,
-      watchedOn: null,
-      isFavorite: true
-    },
-    {
-      id: 3,
-      name: "The Redemption",
-      medium: "Movies",
-      category: "Action",
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    },
-    {
-      id: 4,
-      name: "Hoopers",
-      medium: "Series",
-      category: "Drama",
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    },
-    {
-      id: 5,
-      name: "Happy Joe: Cheery Road",
-      medium: "Movies",
-      category: "Action",
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
-    }
-  ];
-
   constructor(private http: HttpClient) {}
 
   get(medium) {
@@ -63,16 +16,26 @@ export class MediaItemService {
     return this.http.get<MediaItemsResponse>("mediaitems", getOptions).pipe(
       map((response: MediaItemsResponse) => {
         return response.mediaItems;
-      })
+      }),
+      catchError(this.handleError)
     );
   }
 
   add(mediaItem) {
-    return this.http.post("mediaitems", mediaItem);
+    return this.http
+      .post("mediaitems", mediaItem)
+      .pipe(catchError(this.handleError));
   }
 
   delete(mediaItem) {
-    return this.http.delete(`mediaitems/${mediaItem.id}`);
+    return this.http
+      .delete(`mediaitems/${mediaItem.id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error.message);
+    return throwError("A data error occured, please try again");
   }
 }
 interface MediaItemsResponse {
