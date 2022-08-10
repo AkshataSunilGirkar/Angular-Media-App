7992b44971d3695764bceca6a7da59358473fb48
Akshata Girkar <Akshata.Girkar@ril.com>
2022-08-10 21:34:21 +0530

    Adds routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { RouterModule, Routes } from "@angular/router";
+import { MediaItemFormComponent } from "./media-item-form.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+
+const appRoutes: Routes = [
+  { path: "add", component: MediaItemFormComponent },
+  { path: ":medium", component: MediaItemListComponent },
+  { path: "", redirectTo: "all", pathMatch: "full" }
+];
+export const routing = RouterModule.forRoot(appRoutes);
